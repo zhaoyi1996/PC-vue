8471e6aa124ddc91eb78019fcdb2ae84b6e639cd
root <root@localhost.localdomain>
2020-08-15 15:21:50 +0800

    新闻详情页布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Index from '@/components/Index/Index'
+import List from '@/components/Index/List'
+import Details from '@/components/Index/Details'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index
+    },{
+      path: '/list',
+      name: 'List',
+      component: List
+    },{
+      path: '/details',
+      name: 'Details',
+      component: Details
     }
   ]
 })
